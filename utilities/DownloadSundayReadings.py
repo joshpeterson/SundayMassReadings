a321c4f88524dd41e65cc2e1f0b7b1d5ec0df49b
Josh Peterson <petersonjm1@gmail.com>
2016-01-06 06:11:49 -0500

    Update the readings for 2016.
    * Remove the readings for 2012 and 2013 to make the size smaller.
    * Improve the parsing code choose the 1st of two optional readings.

--- utilities/DownloadSundayReadings.py
+++ utilities/DownloadSundayReadings.py
@@ -1,4 +1,5 @@
-# This software is licensed under the MIT License http://www.opensource.org/licenses/mit-license.php.
+# This software is licensed under the MIT License
+# http://www.opensource.org/licenses/mit-license.php.
 # See https://github.com/joshpeterson/SundayMassReadings for more details.
 
 # Based on the code from here: http://stackoverflow.com/questions/2003870/how-can-i-select-all-of-the-sundays-for-a-year-using-python
@@ -14,10 +15,10 @@ def allsundays(year):
 
 base_url = "http://www.usccb.org/bible/readings/"
 
-for d in allsundays(2015):
+for d in allsundays(2016):
    date_parts = str(d).split("-")
    date_string = date_parts[1] + date_parts[2] + date_parts[0][2:4]
    url = base_url + date_string + ".cfm"
    print "Getting " + url
-   urllib.urlretrieve(url, r"raw\\" + date_string + ".cfm")
+   urllib.urlretrieve(url, r"raw/" + date_string + ".cfm")
 
--- utilities/ExtractSundayReadingData.py
+++ utilities/ExtractSundayReadingData.py
@@ -1,4 +1,5 @@
-# This software is licensed under the MIT License http://www.opensource.org/licenses/mit-license.php.
+# This software is licensed under the MIT License
+# http://www.opensource.org/licenses/mit-license.php.
 # See https://github.com/joshpeterson/SundayMassReadings for more details.
 
 from bs4 import BeautifulSoup
@@ -7,15 +8,16 @@ import re
 import sys
 
 def BuildSundayMassReadingJson(html_string, url_string):
-    soup = BeautifulSoup(html_string)
+    soup = BeautifulSoup(html_string, "html.parser")
 
     date = soup.find_all("h1")[0].text
-    title = soup.title.text
+    title = soup.find_all("h3")[2].text
+    title = title[0:title.find("Lectionary")]
 
     readings = soup.find_all("a", { "class" : "book" })
     if len(readings) == 0:
         readings = soup.find_all("div", {"class":"bibleReadingsWrapper"})
-        sub_soup = BeautifulSoup(str(readings))
+        sub_soup = BeautifulSoup(str(readings), "html.parser")
         sub_readings = sub_soup.find_all("a", {"href":re.compile(".*bible.*")})
 
         if len(sub_readings) == 0:
@@ -23,24 +25,31 @@ def BuildSundayMassReadingJson(html_string, url_string):
             return
         readings = sub_readings
 
-    gospelIndex = 3
-    if len(readings) == 5:
-        gospelIndex = 4
-    elif len(readings) != 4:
-        print "Check readings for " + date + " too may readings found."
+    for i in range(0, len(sub_readings)):
+        readingName = sub_readings[i].parent.text
+        if readingName.startswith("Reading 1"):
+            firstReadingIndex = i
+        elif readingName.startswith("Responsorial Psalm"):
+            psalmIndex = i
+        elif readingName.startswith("Reading 2"):
+            secondReadingIndex = i
+        elif readingName.startswith("Gospel"):
+            gospelIndex = i
 
-    firstReading = readings[0].text
-    psalm = readings[1].text
-    secondReading = readings[2].text
-    gospelReading = readings[gospelIndex].text
+    firstReading = readings[firstReadingIndex].text.replace("\\n","").replace("\u2014", "-")
+    psalm = readings[psalmIndex].text.replace("\\n","").replace("\u2014", "-")
+    secondReading = readings[secondReadingIndex].text.replace("\\n","").replace("\u2014", "-")
+    gospelReading = readings[gospelIndex].text.replace("\\n","").replace("\u2014", "-")
 
     json_entry = "\"" + date + "\"" + ":{title:\"" + title + "\",url:\"" + url_string + "\",first:\"" + firstReading + "\",psalm:\"" + psalm + "\",second:\"" + secondReading + "\",gospel:\"" + gospelReading + "\"},"
     print json_entry
 
-for file_name in glob.glob("*.cfm"):
+for file_name in sorted(glob.glob("*.cfm")):
     f = open(file_name);
     try:
         BuildSundayMassReadingJson(f.read(), file_name[:6])
+    except KeyboardInterrupt:
+        raise
     except:
         print "Exception parsing " + file_name + "\n" + str(sys.exc_info()[0])
 
